44a04ad698445c199fd82b6c58f86d865753488e
ronylicha <ronylicha@gmail.Com>
2024-02-05 15:19:08 +0100

    Add setUp method to CheckBoxImages component
    Added a setUp method to the CheckBoxImages component. This helps to ensure that any preparatory actions required by the parent Checkbox class are properly executed before CheckBoxImages-specific tasks.

--- src/Actions/CheckBoxImages.php
+++ src/Actions/CheckBoxImages.php
@@ -7,4 +7,9 @@ use Filament\Forms\Components\Checkbox;
 class CheckBoxImages extends Checkbox
 {
     protected string $view = 'checkboximages::components.checkbox-button-image';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }
