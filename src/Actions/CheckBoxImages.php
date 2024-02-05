c8a6d30c427bbb9df3234b0bfad85d6e281d102e
ronylicha <ronylicha@gmail.Com>
2024-02-05 15:07:05 +0100

    Update base class for CheckBoxImages component
    The CheckBoxImages component was previously extending the Radio class from the Filament Forms package. In this update, the base class has been changed and it now extends the Checkbox class instead. This change is expected to bring the more consistent behavior expected of a checkbox, as indicated by its name.

--- src/Actions/CheckBoxImages.php
+++ src/Actions/CheckBoxImages.php
@@ -2,9 +2,9 @@
 
 namespace ronylicha\CheckBoxImages\Actions;
 
-use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Checkbox;
 
-class CheckBoxImages extends Radio
+class CheckBoxImages extends Checkbox
 {
     protected string $view = 'checkboximages::components.checkbox-button-image';
 }
